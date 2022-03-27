0bd568965a0c2b53af891ec4661df1239293c37b
Mahin Ahmed <XenonTheInertG@outlook.com>
2022-03-27 09:36:57 +0600

    OrangeFox-CI: Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# 🦊 OrangeFox Recovery CI
+A Free CI to Build OrangeFox Recovery
+
+### - Build via a PR (Pull Request)
+1. Fork [this Repo](https://github.com/XenonTheInertG-CI/OrangeFox-CI.git).
+2. Edit ```config.sh``` (Only) According to your Needs.
+3. Create a Pull Reuest.
+4. Go to [cirrus-ci.com](https://cirrus-ci.com) And see the Build Logs.
---
+++ config.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Device
+export FOX_BRANCH="fox_9.0"
+export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+
+export DEVICE="beryllium"
+export OEM="xiaomi"
+export TARGET="recoveryimage"
+
+export OUTPUT="OrangeFox*.zip"
+
+# Kernel Source
+# Uncomment the next line if you want to clone a kernel source.
+#export KERNEL_SOURCE="https://gitlab.com/OrangeFox/kernel/mojito.git"
+#export PLATFORM="sm6150" # Leave it commented if you want to clone the kernel to kernel/$OEM/$DEVICE
+
+# Extra Command
+export EXTRA_CMD="git clone https://github.com/OrangeFoxRecovery/Avatar.git misc"
+
+# Not Recommended to Change
+export SYNC_PATH="$HOME/work" # Full (absolute) path.
+export USE_CCACHE=1
+export CCACHE_SIZE="50G"
+export CCACHE_DIR="$HOME/work/.ccache"
+export J_VAL=16
+
+if [ ! -z "$PLATFORM" ]; then
+    export KERNEL_PATH="kernel/$OEM/$PLATFORM"
+else
+    export KERNEL_PATH="kernel/$OEM/$DEVICE"
+fi
+export DT_PATH="device/$OEM/$DEVICE"
