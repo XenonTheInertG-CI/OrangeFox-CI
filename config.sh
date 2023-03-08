2fb549755ec84076f2940ab943dc710c976de23a
plente00 <123705363+plente00@users.noreply.github.com>
2023-03-08 12:35:42 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_11.0"
-export DT_LINK="https://github.com/clhexareslab/recovery_device_xiaomi_ares.git -b fox_11.0"
+export FOX_BRANCH="fox_6.0"
+export DT_LINK="https://github.com/plente00/android_device_sony_bahamut.git -b android-10"
 
-export DEVICE="ares"
-export OEM="xiaomi"
+export DEVICE="bahamut"
+export OEM="sony"
 export TARGET="bootimage"
 
 export OUTPUT="OrangeFox*.zip"
